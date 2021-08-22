ec886683bc1130fa32beebab0e2fecabf8543572
Masudha Meher <kasturimeh@gmail.com>
2021-08-22 19:46:02 +0530

    changed contact form layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,103 +25,59 @@ export default function Contact() {
   }
   return (
     <div className="ContactMe" id="contact">
-      <h1 className="contact-header">
-        Contact Me
-      </h1>
-      <hr
-        className="contact-divider"
-      />
-      <Container className="contact-content">
-        <Col>
-          <Row>
-            <Col>
-              <Row>
-                <Col md="auto" className="contact-logo">
-                  <img
-                    src="images/mail.svg"
-                    alt="email-icon"
-                    height="50"
-                    width="50"
-                  />
-                </Col>
-                <Col md="auto">
-                  <Row className="contact-heading">Email</Row>
-                  <Row className="contact-value">kasturimeh@gmail.com</Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Row>
-                <Col md="auto" className="contact-logo">
-                  <img
-                    src="images/phone.svg"
-                    alt="email-icon"
-                    height="50"
-                    width="50"
-                  />
-                </Col>
-                <Col md="auto">
-                  <Row className="contact-heading">Phone</Row>
-                  <Row className="contact-value">+91-9711843904</Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <div className="form-group-total">
-            <form onSubmit={sendEmail}>
-              <div class="row">
-                <div class="form-group name1 col-md-6">
-                  <input
-                    type="text"
-                    className="form-control form-control-md"
-                    id="name"
-                    placeholder="Enter your name"
-                    name="name"
-                    required="required"
-                  />
-                </div>
-                <br/>
-                <div class="form-group name1 col-md-6">
-                  <input
-                    type="text"
-                    className="form-control form-control-md"
-                    id="email"
-                    placeholder="Your email address"
-                    name="email"
-                    required="required"
-                  />
-                </div>
+      <h1 className="contact-header">Contact Me</h1>
+      <hr className="contact-divider" />
+      <div className="contact-content">
+        <div className="form-group-total">
+          <form onSubmit={sendEmail}>
+            <div class="row">
+              <div class="form-group name1 col-md-6">
+                <input
+                  type="text"
+                  className="form-control form-control-md"
+                  id="name"
+                  placeholder="Enter your name"
+                  name="name"
+                  required="required"
+                />
               </div>
-              <br/>
-              <input
-                type="text"
-                className="form-control form-control-md"
-                id="discussion-title"
-                placeholder="Enter the discussion title"
-                name="subject"
-                required="required"
-              />
               <br />
-              <textarea
-                className="form-control"
-                name="message"
-                id="message"
-                style={{ height: 100 }}
-                placeholder="Write your message"
-                required="required"
-              ></textarea>
-              <br />
-              <button type="submit" className="contact-button">
-                Send Message Now
-              </button>
-            </form>
-          </div>
-        </Col>
-      </Container>
+              <div class="form-group name1 col-md-6">
+                <input
+                  type="text"
+                  className="form-control form-control-md"
+                  id="email"
+                  placeholder="Your email address"
+                  name="email"
+                  required="required"
+                />
+              </div>
+            </div>
+            <br />
+            <input
+              type="text"
+              className="form-control form-control-md"
+              id="discussion-title"
+              placeholder="Enter the discussion title"
+              name="subject"
+              required="required"
+            />
+            <br />
+            <textarea
+              className="form-control"
+              name="message"
+              id="message"
+              style={{ height: 100 }}
+              placeholder="Write your message"
+              required="required"
+            ></textarea>
+            <br />
+            <button type="submit" className="contact-button">
+              Send Message Now
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
