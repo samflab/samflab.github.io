d431b12cd6691f9450e6999db01ef4d4811d1d74
Masudha Meher <kasturimeh@gmail.com>
2022-03-27 16:11:23 +0530

    refactor: class to function component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import ProjectCard from "./cards/ProjectCard";
-class ProjectWork extends Component {
-  render() {
-    return (
-      <div className="Project" id="projects">
-        <h1 className="project-header">
-          Projects
-        </h1>
-        <ProjectCard />
-      </div>
-    );
-  }
-}
+const ProjectWork = () => {
+  return (
+    <div className="Project" id="projects">
+      <h1 className="project-header">Projects</h1>
+      <ProjectCard />
+    </div>
+  );
+};
 
 export default ProjectWork;
