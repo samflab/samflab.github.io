eb8ed60ba14f3851048096c7243e4b4a6b4fecb9
Masudha Meher <kasturimeh@gmail.com>
2022-03-27 16:30:43 +0530

    refactor: blogs section

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,200 +1,54 @@
 import "../styles/Blog.scss";
 import { FaMediumM } from "react-icons/fa";
 import { SiHashnode } from "react-icons/si";
+import { blogDetails } from "../details/blogDetails";
 
-function blogLinkClick(link) {
-  const url = link;
-  window.open(url, "_blank");
-}
+const blogLinkClick = (link) => window.open(link, "_blank");
 
-function Blog() {
+const Blog = () => {
   return (
     <div className="Blog" id="blogs">
       <h1 className="blog-header">Blogs</h1>
       <hr className="blog-divider" />
       <div>
         <div className="blog-plate">
-          <div className="blog-layout">
-            <span className="blog-body">
-              <h4 className="blog-title">
-                Hoisting in JavaScript
-              </h4>
-              <p className="blog-desc">
-              One of the hot topics around JavaScript is hoisting.
-              We'll understand what is hoisting. Let's dive deep into this. Hoisting in Javascript is a mechanism...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-                      "https://samflab.medium.com/hoisting-in-javascript-var-let-and-const-e3b70c536f55"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-                <button className="urls"
-                onClick={() => {
-                  blogLinkClick(
-                    "https://hashnode.com/post/hoisting-in-javascript-ckycnb7sv01jbo7s13a2n9eky"
-                  );
-                }}
-                >
-                  <SiHashnode />
-                </button>
+          {blogDetails.map((blog) => {
+            return (
+              <div className="blog-layout">
+                <span className="blog-body">
+                  <h4 className="blog-title">{blog.title}</h4>
+                  <p className="blog-desc">{blog.desc}</p>
+                  <div>
+                    <button className="read-more">Read More Here: </button>
+                    <button
+                      className="urls"
+                      onClick={() => {
+                        blogLinkClick(blog.bloglink1);
+                      }}
+                    >
+                      <FaMediumM />
+                    </button>
+                    {blog.bloglink2 ? (
+                      <button
+                        className="urls"
+                        onClick={() => {
+                          blogLinkClick(blog.bloglink2);
+                        }}
+                      >
+                        <SiHashnode />
+                      </button>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </span>
               </div>
-            </span>
-          </div>
-          <div className="blog-layout">
-            <span className="blog-body">
-              <h4 className="blog-title">
-                Send Emails using ReactJS from EmailJS
-              </h4>
-              <p className="blog-desc">
-                Yes, you read it right ! Finally a functionality that lets you
-                send email from your ReactJS based frontend to your mail...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-                      "https://samflab.medium.com/send-emails-using-reactjs-from-emailjs-c0a64a57e36b"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-                <button className="urls"
-                onClick={() => {
-                  blogLinkClick(
-                    "https://samflab.hashnode.dev/send-emails-using-reactjs-from-emailjs"
-                  );
-                }}
-                >
-                  <SiHashnode />
-                </button>
-              </div>
-            </span>
-          </div>
-          <div className="blog-layout">
-            <span className="blog-body">
-              <p className="blog-title">
-                Smallest Positive Missing Number in an Unsorted Array
-              </p>
-              <p className="blog-desc">
-                We have an array consisting of negative and positive numbers, we
-                have to finding the smallest missing positive number from the
-                given array...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-                      "https://samflab.medium.com/smallest-positive-missing-number-in-an-unsorted-array-8f8a0439fb09"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-              </div>
-           
-            </span>
-          </div>
-          <div className="blog-layout">
-            <span className="blog-body">
-              <h4 className="blog-title">
-                Inserting Node into a Sorted Doubly Linked List
-              </h4>
-              <p className="blog-desc">
-                Recently, I was solving some coding questions on Hackerrank and
-                I came across this problem. I realised that this problem
-                lacked...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-              "https://samflab.medium.com/inserting-node-into-a-sorted-doubly-linked-list-67c638c4ec21"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-              </div>
-           
-            </span>
-          </div>
-          <div className="blog-layout">
-            <span className="blog-body">
-              <h4 className="blog-title">Responsive Text in Flutter</h4>
-              <p className="blog-desc">
-                Have you ever come across a situation where you realized you
-                have to resize your text according to different screen sizes?
-                Unfortunately...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-              "https://samflab.medium.com/responsive-text-in-flutter-744fc830a9cd"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-              </div>
-              
-            </span>
-          </div>
-          <div className="blog-layout">
-            <span className="blog-body">
-              <h4 className="blog-title">Our Train of Thoughts</h4>
-              <p className="blog-desc">
-                It might seem like a vague question but have you ever put some
-                thought into it? Being consumed by our thoughts and our thoughts
-                consuming us...
-              </p>
-              <div>
-                <button className="read-more">Read More Here: </button>
-                &nbsp; &nbsp;
-                <button
-                  className="urls"
-                  onClick={() => {
-                    blogLinkClick(
-              "https://samflab.medium.com/us-and-our-thoughts-76321f6a7801"
-                    );
-                  }}
-                >
-                  <FaMediumM />
-                </button>
-                &nbsp;
-              </div>
-            </span>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Blog;
---
+++ src/details/blogDetails.js
@@ -0,0 +1,41 @@
+export const blogDetails = [
+  {
+    title: "Hoisting in JavaScript",
+    desc: " One of the hot topics around JavaScript is hoisting. We'll understand what is hoisting. Let's dive deep into this. Hoisting in Javascript is a mechanism...",
+    bloglink1:
+      "https://samflab.medium.com/hoisting-in-javascript-var-let-and-const-e3b70c536f55",
+    bloglink2:
+      "https://hashnode.com/post/hoisting-in-javascript-ckycnb7sv01jbo7s13a2n9eky",
+  },
+  {
+    title: "Send Emails using ReactJS from EmailJS",
+    desc: "Yes, you read it right ! Finally a functionality that lets you send email from your ReactJS based frontend to your mail...",
+    bloglink1:
+      "https://samflab.medium.com/send-emails-using-reactjs-from-emailjs-c0a64a57e36b",
+    bloglink2:
+      "https://samflab.hashnode.dev/send-emails-using-reactjs-from-emailjs",
+  },
+  {
+    title: "Smallest Positive Missing Number in an Unsorted Array",
+    desc: "We have an array consisting of negative and positive numbers, we have to finding the smallest missing positive number from the given array...",
+    bloglink1:
+      "https://samflab.medium.com/smallest-positive-missing-number-in-an-unsorted-array-8f8a0439fb09",
+  },
+  {
+    title: "Inserting Node into a Sorted Doubly Linked List",
+    desc: "Recently, I was solving some coding questions on Hackerrank and I came across this problem. I realised that this problem lacked...",
+    bloglink1:
+      "https://samflab.medium.com/inserting-node-into-a-sorted-doubly-linked-list-67c638c4ec21",
+  },
+  {
+    title: "Responsive Text in Flutter",
+    desc: " Have you ever come across a situation where you realized you have to resize your text according to different screen sizes? Unfortunately...",
+    bloglink1:
+      "https://samflab.medium.com/responsive-text-in-flutter-744fc830a9cd",
+  },
+  {
+    title: "Our Train of Thoughts",
+    desc: "It might seem like a vague question but have you ever put some thought into it? Being consumed by our thoughts and our thoughts consuming us...",
+    bloglink1: "https://samflab.medium.com/us-and-our-thoughts-76321f6a7801",
+  },
+];
