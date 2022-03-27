01bef394d71a7286604c6d7b351eac0c83bbf066
Masudha Meher <kasturimeh@gmail.com>
2022-03-27 17:14:11 +0530

    refactor: internship

--- src/components/Internship.js
+++ src/components/Internship.js
@@ -1,24 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "../components/cards/InternshipCard";
 import "../styles/Resume.scss";
 
-class Internship extends Component {
-  render() {
-    return (
-      <div id="internship">
-        <h3
-          style={{
-            fontWeight: "700",
-            marginBottom: "30px",
-            fontSize: "24px",
-            fontFamily: "Poppins",
-          }}
-        >
-          Internship
-        </h3>
-        <Card />{" "}
-      </div>
-    );
-  }
-}
+const Internship = () => {
+  return (
+    <div id="internship">
+      <h3 className="resume-sub-heading">Internship</h3>
+      <Card />
+    </div>
+  );
+};
 export default Internship;
